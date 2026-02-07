e8f90e091ac772ff2b777a726440341acd56d9e6
siv727 <verinkai727@gmail.com>
2026-02-07 09:30:10 +0800

    feat(User): add UserRepository interface for user data access

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -1,10 +1,12 @@
 package com.example.backend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
     public Long id;
     public String firstname;
