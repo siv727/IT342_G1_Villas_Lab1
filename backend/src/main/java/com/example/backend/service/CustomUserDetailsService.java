c818e249005ffa865fe822a4fa7e189ea155e9e7
siv-a-siv <verinkai727@gmail.com>
2026-02-14 00:58:45 +0800

    feat(CustomUserDetailsService): implement UserDetailsService for user authentication

---
+++ backend/src/main/java/com/example/backend/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.backend.repository.UserRepository;
+import com.example.backend.entity.User;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
